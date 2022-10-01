d8764c576f5e4aa75084047513293a2ba8014441
jeff <jeffmuigai11@gmail.com>
2022-10-01 12:39:58 +0300

    FEAT: INIT CRYPTO PROJECT (CRYPTO GIANT)
    PROJECT GOALS:
    
    1.  Build a front-end application utilizing a crypto api to well
        visualize the all cryptocurrencies and the detailed individual
        coin analysis
    
    2.  Provide a crypto news page with the capability of filtering by key
        words

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="#" target="_blank">
+          <img
+            src="/images/cryto-giant-rec.png"
+            className="logo"
+            alt="Crypto Giant"
+          />
+        </a>
+      </div>
+      <h1>Crypto Giant</h1>
+    </div>
+  );
+}
+
+export default App;
