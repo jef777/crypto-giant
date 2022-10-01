7f9b3092cb8c97fbca49d0a39b39bfee9f057bbc
jeff <jeffmuigai11@gmail.com>
2022-10-01 13:45:35 +0300

    FEAT: Config Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="#" target="_blank">
-          <img
-            src="/images/cryto-giant-rec.png"
-            className="logo"
-            alt="Crypto Giant"
-          />
-        </a>
-      </div>
-      <h1>Crypto Giant</h1>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <a href="#" target="_blank">
+        <img
+          src="/images/cryto-giant-rec.png"
+          className="logo"
+          alt="Crypto Giant"
+        />
+      </a>
+      <h1 className=" font-bold text-lg">Crypto Giant</h1>
     </div>
   );
 }
