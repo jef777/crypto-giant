7d4f9ce8abb3eef17d995a871d33ae424c938926
jeff <jeffmuigai11@gmail.com>
2022-12-06 19:56:58 +0300

    Update menu links

--- src/templates/menu/menu_links.js
+++ src/templates/menu/menu_links.js
@@ -1,6 +1,7 @@
 import {
   ComputerDesktopIcon,
   CircleStackIcon,
+  NewspaperIcon,
 } from '@heroicons/react/24/outline';
 
 export const navigationLinks = [
@@ -9,6 +10,12 @@ export const navigationLinks = [
     name: 'crypto',
     to: '/cryptocurrencies',
     icon: CircleStackIcon,
+    current: false,
+  },
+  {
+    name: 'crypto-news',
+    to: '/crypto-news',
+    icon: NewspaperIcon,
     current: false,
   },
 ];
